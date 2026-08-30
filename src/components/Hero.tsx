7c9a5e30589a71e6acebb287c09f3506f864a67a
iroha <y-kobayashi@fusic.co.jp>
2026-08-30 14:31:32 +0900

    feat: ヒーローを追加

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <div>
+      <p className="text-success">// GAME_DEV_BLOG</p>
+      <h1>Unityの実践的な知識を記録する</h1>
+      <p className="text-secondary">
+        VFXGraph / アセット類 / Shadergraph / ゲーム数学など
+      </p>
+    </div>
+  );
+}
