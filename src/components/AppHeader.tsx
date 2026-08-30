636e8be7342d9cef5c07bf91c74b6959930fbcc9
iroha <y-kobayashi@fusic.co.jp>
2026-08-30 16:58:05 +0900

    refactor: 不要なファイルの削除とタイトルを修正

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,7 +2,7 @@ export default function AppHeader() {
   return (
     <nav className="navbar bg-body-tertiary">
       <div className="container">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
           ARTS -Unity Devlog-
         </a>
         <span className="navbar-text">v{__APP_VERSION__}</span>
